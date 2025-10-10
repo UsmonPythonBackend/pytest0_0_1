77f25fd93e809ad544154c84943f2157b1097f30
Usmonjon Axmadaliyev <166992108+UsmonPythonBackend@users.noreply.github.com>
2025-10-10 15:24:15 +0500

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -7,3 +7,8 @@ def test_env():
     base_url = os.getenv('BASE_URL')
 
     print(base_url)
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if not BOT_TOKEN or not CHAT_ID:
+    print("⚠️ Внимание: TELEGRAM_BOT_TOKEN не задан.")
